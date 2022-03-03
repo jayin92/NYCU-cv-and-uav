d81d51c11b3d522ee1dd3a3b67f3bb4761996dfa
jayin92 <jayin920805@gmail.com>
2022-03-03 17:16:51 +0800

    temp

--- lab01/bilinear.py
+++ lab01/bilinear.py
@@ -16,6 +16,16 @@ for i in range(h):
 
 res = np.zeros((3*h, 3*w, 3))
 
+for i in range(3*h):
+    for j in range(3*w):
+        x = (i-1) // 3 + 1
+        y = (j-1) // 3 + 1
+        dx = i - (i-1) // 3 * 3
+        dy = j - (j-1) // 3 * 3
+        dx /= 3
+        dy /= 3
+        
+
 
 
 cv2.imwrite("lisa_bilinear.jpeg", res)
